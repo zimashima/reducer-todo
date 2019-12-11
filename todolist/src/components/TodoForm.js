e1873d8a8ba6ea65fe1ba5d50120caea5bc10fde
Mashima <52222646+zimashima@users.noreply.github.com>
2019-12-11 09:46:09 -0500

    played around with styling and get the main idea of what it should be

--- todolist/src/components/TodoForm.js
+++ todolist/src/components/TodoForm.js
@@ -6,13 +6,18 @@ export default function TodoForm(props){
     return(
         <div>
             <form onSubmit={props.handleSubmit}>
-                <div class="form-group">
-                    <input type="text" class="form-control" id="addTodo" placeholder="Add a new task" onChange={props.handleChanges} value={props.newTask}/>
-                    <input type="text" class="form-control" id="addDate" placeholder="Add complete by date" onChange={props.handleDateText} value={props.dateText} />
-                    <div className="d-flex justify-content-center"><button type="submit" className="btn btn-primary m-2">Add Task</button>
-                    <button className="btn btn-secondary m-2" onClick={props.handleCompleted}>Clear Completed</button>
+                <div class="form-group d-flex justify-content-between">
+                    <div className="col-md-9">
+                        <input type="text" class="form-control" id="addTodo" placeholder="Add a new task" onChange={props.handleChanges} value={props.newTask}/>
+                    </div>
+                    <div className="col-md-3">
+                        <input type="text" class="form-control" id="addDate" placeholder="Add complete by date" onChange={props.handleDateText} value={props.dateText} />
                     </div>
                 </div>
+                <div className="d-flex justify-content-center"><button type="submit" className="btn btn-primary m-2">Add Task</button>
+                    <button className="btn btn-secondary m-2" onClick={props.handleCompleted}>Clear Completed</button>
+                </div>
+            
             </form>
         </div>
     )
